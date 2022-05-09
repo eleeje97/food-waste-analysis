e7f3f97eb00ca892a48bc0abf2b82dc30805a61a
danalee <eleeje97@gmail.com>
2022-05-09 11:55:01 +0900

    waste1920.csv

--- lde/temp3.R
+++ lde/temp3.R
@@ -13,7 +13,28 @@ for (i in 1:nrow(waste19)) {
   }
 }
 waste19 <- waste19 %>% filter(시도 == "경기") %>% 
-  filter(폐기물종류 == "음식물류 폐기물 분리배출" | 폐기물종류_세부 == "음식물류 폐기물")
-waste19 <- waste19 %>% group_by(시도, 시군구) %>% summarise(배출량=sum(배출량)) %>% 
+  filter(폐기물종류 == "음식물류 폐기물 분리배출" | 폐기물종류_세부 == "음식물류 폐기물") %>% 
+  select(-시도)
+waste19 <- waste19 %>% group_by(시군구) %>% summarise(배출량=sum(배출량)) %>% 
   filter(!시군구=="소계")
 waste19$년도 <- "2019"
+
+
+library(readr)
+waste20 <- read_csv("../yjh/data/sgg_waste_2020.csv")
+waste20 <- waste20 %>% filter(!(signgu=="소계" | signgu=="합계"))
+colnames(waste20) <- c("시군구", "배출량")
+waste20$년도 <- "2020"
+
+
+waste1920 <- rbind(waste19, waste20)
+ggplot(data = waste1920,
+       aes(x = 시군구, y = 배출량, fill = 년도)) +
+  geom_col(position = "dodge") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# 시군구별 증감률
+waste1920 <- cbind(select(waste19, c(시군구, 배출량)), select(waste20, 배출량))
+colnames(waste1920) <- c("시군구", "2019", "2020")
+waste1920$rate <- (waste1920$`2020`-waste1920$`2019`)/waste1920$`2019` * 100
+
