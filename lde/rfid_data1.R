55359f72d361d3940443dd4eb01ade4ec221b894
danalee <eleeje97@gmail.com>
2022-05-10 12:07:24 +0900

    rfid_swpc

--- lde/rfid_data1.R
+++ lde/rfid_data1.R
@@ -30,9 +30,18 @@ getResponse3 <- function(sigungu_name) {
 
 
 # API 10번 지자체별 배출내역(일별) 목록 조회 함수
-getResponse10 <- function(cityCode) {
-  res <- GET(url = paste(url, "getCityDateList"))
+getResponse10 <- function(disYear, disMonth, cityCode) {
+  res <- GET(url = paste(url, "getCityDateList", sep = ""),
+             query = list(ServiceKey=api_key,
+                          type="json",
+                          disYear=disYear,
+                          disMonth=disMonth,
+                          cityCode=cityCode,
+                          rowNum=31))
+  res_df <- res %>% content(as = "text", encoding = "UTF-8") %>% fromJSON()
+  return(res_df$data$list)
 }
+
 
 # API 15번 아파트 세대수 및 좌표 목록 조회 함수
 getResponse15 <- function(cityCode) {
@@ -65,5 +74,45 @@ for (i in 1:length(gyeonggi_sigungu_10)) {
 colnames(cityCodes) <- c("sigungu", "citycode")
 
 
+
+## 수원, 포천 배출내역 뽑기 ##
+library(stringr)
+cityCodes_sw_pc <- cityCodes %>% filter(str_detect(sigungu, "수원") | sigungu == "포천시")
+year <- c("2017", "2018", "2019", "2020", "2021")
+rfid_swpc <- data.frame()
+for (i in 1:length(year)) {
+  for (j in 1:length(cityCodes_sw_pc$citycode)) {
+    for (k in 1:12) {
+      month <- formatC(k, width = 2, format = "d", flag = "0")
+      temp <- getResponse10(year[i], month, cityCodes_sw_pc$citycode[j])
+      rfid_swpc <- rbind(rfid_swpc, temp)
+    }
+  }
+}
+
+
+rfid_swpc <- rfid_swpc %>% select(disYear, disMonth, cityCode, citySggName, disQuantity)
+rfid_swpc <- rfid_swpc %>% arrange(disYear)
+temp <- rfid_swpc %>% group_by(disYear, disMonth, cityCode, citySggName) %>% 
+  summarise(disQuantity=sum(disQuantity))
+rfid_swpc_2017 <- temp %>% filter(disYear==2017)
+rfid_swpc_2018 <- temp %>% filter(disYear==2018)
+rfid_swpc_2019 <- temp %>% filter(disYear==2019)
+rfid_swpc_2020 <- temp %>% filter(disYear==2020)
+rfid_swpc_2021 <- temp %>% filter(disYear==2021)
+
+
+# csv 파일로 저장
+# write.csv(rfid_swpc, "data/rfid_swpc.csv", row.names = F)
+# write.csv(rfid_swpc_2017, "data/rfid_swpc_2017.csv", row.names = F)
+# write.csv(rfid_swpc_2018, "data/rfid_swpc_2018.csv", row.names = F)
+# write.csv(rfid_swpc_2019, "data/rfid_swpc_2019.csv", row.names = F)
+# write.csv(rfid_swpc_2020, "data/rfid_swpc_2020.csv", row.names = F)
+# write.csv(rfid_swpc_2021, "data/rfid_swpc_2021.csv", row.names = F)
+
+
+
+
 ## 아파트 세대수 및 좌표 뽑기 ##
 a <- getResponse15(cityCodes$citycode[1])
+
